48ca66b160b94a93955b881b49bbd0556e28b8b8
danix <angeldaniel6709@gmail.com>
2024-03-03 17:03:09 -0500

    Add @midudev/tailwind-animations package and update dependencies

---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="flex  flex-row">
+        <div class="buscar navbar w-[300px] h-[5rem] hidden bg-blue-intense cursor-pointer rounded-es  justify-center items-center">
+            <input class="w-full pl-[10px] h-full bg-blue-intense text-white" type="search" placeholder="Busca en UG">
+        </div>
+        <div class="navbar w-[10rem] h-[5rem] bg-blue-intense rounded-es flex cursor-pointer justify-center items-center">
+            <img src="./../../public/search.svg" width="" height="" alt="buscar">
+            <p class=" searchIcon text-roboto text-[20px] ml-2 text-white-light">Buscar</p>
+        </div>
+        <div class="buscar navbar w-[10rem] h-[5rem] bg-blue-intense cursor-pointer flex justify-center items-center">
+            <img src="./../../public/menu.svg" width="" height="" alt="buscar">
+            <p class="text-roboto text-[20px] ml-2 text-white-light">Menu</p>
+        </div>
+    </section>
+
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+input::placeholder {
+  padding-left: 2px; /* Añade un "margen" al inicio del placeholder */
+}
+</style>
--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,32 +1,33 @@
 <template>
     <div class="absolute h-screen w-screen z-30 top-0 left-0 flex items-center justify-center">
-       <transition name="fade">
-        <h1 class="font-[Lora] font-normal text-5xl text-center ">
-            la mas <span class="text-blue-intense font-bold ">{{title}}</span> del Ecuador
+        <h1 class="font-[Lora] font-normal text-6xl text-center animate-blurred-fade-in" >
+            La mas <span  v-bind:class="title==='gloriosa'?'text-blue-light':(title==='importante'?'text-blue-mid':'text-blue-intense')" :class="isChange? 'animate-fade-in':''">{{title}}</span> del Ecuador
         </h1>
-       </transition>
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 const title = ref<string>("grande");
+const options = ref(["grande","gloriosa","importante"])
+let index = 1
+const isChange = ref(false);
 
-const changeTitle = (newTitle:string)=>{
-    title.value = newTitle;
+const changeTitle = ()=>{
+  title.value = options.value[index];
+  index = index === 2 ? 0 : index + 1;
 }
 setInterval(()=>{
-    changeTitle("ref");
+    changeTitle();
 }, 3000);
-    
+
+watch(title, ()=>{
+  isChange.value = true;
+  new Promise(resolve => setTimeout(resolve, 500)) // Espera 0.5 segundos (la duración de la animación)
+    .then(() => isChange.value = false);
+})
 </script>
 
 
 <style scoped>
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
-}
 </style>
