1b9fd1623f78287f054d3638650038ff5ebab73e
danix <angeldaniel6709@gmail.com>
2024-03-03 14:30:08 -0500

    Add new files and configurations

---
+++ src/components/Title.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="absolute h-screen w-screen z-30 top-0 left-0 flex items-center justify-center">
+       <transition name="fade">
+        <h1 class="font-[Lora] font-normal text-5xl text-center ">
+            la mas <span class="text-blue-intense font-bold ">{{title}}</span> del Ecuador
+        </h1>
+       </transition>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+const title = ref<string>("grande");
+
+const changeTitle = (newTitle:string)=>{
+    title.value = newTitle;
+}
+setInterval(()=>{
+    changeTitle("ref");
+}, 3000);
+    
+</script>
+
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
---
+++ tailwind.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors:{
+				'blue-intense':'#0D3F72',
+				'blue-light':'#0CB4D9',
+				'white-light':'#F2F2F2',
+				'white-intense':'#BBC9D2',
+				'light-gray':'#E1DBD9',
+				'blue-mid':'#1776CA',
+				'blulight':'#8AA8BD'
+			},
+			fontFamily: {
+				'body': ['Inter', 'sans-serif'],
+				'heading':['Lora', 'serif']
+			},
+
+		},
+	},
+	plugins: [],
+}
